a2206676c86d72d64e9e94e8acc47277e21af00a
guseul <hbbr@kaist.ac.kr>
2019-07-02 19:22:22 +0900

    SetUp & Create TapNav

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { AppLoading, Font } from "expo";
+import TabNavigation from "./navigation/TabNavigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    loaded: false
+  };
+
+  handleError = error => console.log(error);
+
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+  };
+
+  render() {
+    const { loaded } = this.state;
+    if (loaded) {
+      console.log("ready");
+      return <TabNavigation />;
+    } else {
+      console.log("not yet");
+      return (
+        <AppLoading
+          startAsync={this.loadAssets}
+          onFinish={this.handleLoaded}
+          onError={this.handleError}
+        />
+      );
+    }
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
